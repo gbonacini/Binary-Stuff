d75ea109685b8d797c397340e89be544f493814e
BG <bg@bg.local>
2018-12-17 00:44:00 +0100

    Added: webserver demo x86-64.

---
+++ httpserver/README.md
@@ -0,0 +1,8 @@
+Description:
+============
+
+This is a demo http server written in x86-64 assembly. It listens on tcp port 8880 and responds with a minimal 'hello world" html page.
+Tested on Ubuntu 18.04.1 LTS.
+
+make<BR>
+./websrv<BR>
---
+++ httpserver/websrv.asm
@@ -0,0 +1,78 @@
+;shell.asm
+
+[SECTION .text]
+global _start
+_start:
+          mov       rax,      41                ; socket syscall (41)
+	  mov       rdi,      2                 
+	  mov       rsi,      1                 
+          xor       rdx,      rdx               
+          syscall                               
+
+          xchg      rax,      rdi               
+          mov       r9,       rdi               
+
+          mov       rax,      54                ; setsockopt syscall (54)
+          mov       rsi,      1                 
+          mov       rdx,      2                 
+          mov       r10,      on                
+          mov       r8,       1                 
+          syscall                               
+          
+          mov       dx,       0xB022            
+          shl       rdx,      16                
+          xor       dl,       2                 
+          push      rdx          
+          mov       rsi,      rsp
+
+          mov       rdx,      16                
+          mov       rax,      49                ; bind syscall (49)
+          syscall                               
+          
+                                                
+          mov       rsi,      5                 
+          mov       rax,      50                ; listen syscall (50)
+          syscall                               
+
+          jmp short ender
+
+          starter:
+
+                                                
+          xor       rsi,      rsi               
+          xor       rdx,      rdx               
+          mov       rax,      43                ; accept syscall (43)
+          syscall                               
+
+          mov       rdi,      rax               
+
+          mov       rsi,      buff
+          mov       rdx,      512
+
+          xor       rax,      rax               ; read  syscall (0)
+          syscall                               
+
+          mov       rsi,      page
+          mov       rdx,      pagelen
+
+          mov       rax,      1                 ; write  syscall (1)
+          syscall                               
+
+                                                
+          mov       rax,      3                 ; close syscall (3)
+          syscall                               
+
+          mov       rdi,      r9                
+
+
+          ender:
+          call starter	                        
+
+[SECTION .rodata]
+          page:     db        "HTTP/1.1 200 OK",10,10,"<html><header></header><body>Hello world</body></html>"
+          pagelen:  equ $ -   page
+          on:       db        1
+
+[section .bss]
+          buff resb 512
+
