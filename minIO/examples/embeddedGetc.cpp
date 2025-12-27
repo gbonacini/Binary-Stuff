e2f18517b80e3bc45e233ea092beb4edf158af1c
GBonacini <gabriele.bonacini@protonmail.com>
2025-12-27 07:39:33 +0100

    Cleaning

--- minIO/examples/embeddedGetc.cpp
+++ minIO/examples/embeddedGetc.cpp
@@ -8,15 +8,21 @@ int main(void){
          minIO::exit;
 
    const char* msg { "press 'return' to exit\n" };
-
    printScreen(msg, strnlen(msg, 24));
+
    while(true){
        char in { getChar() };
-       if(in == '\n') break;
-       printScreen(&in, 1);
+       switch(in){
+         case 0:
+              printScreen("\nERR\n", 5);
+              exit();
+         case '\n':
+              printScreen("\nEND\n", 5);
+              exit();
+         default:
+           printScreen(&in, 1);
+       }
    }
-   printScreen("\nEND\n", 5);
 
-   exit();
 }
 
